7792cd17880560cbeed0f0ce60285890a26f2590
deab <deab@skidata.com>
2019-11-21 18:59:11 +0530

    Enabled eureka client

--- src/main/java/com/target/retail/tgtproductdetails/TgtProductDetailsApplication.java
+++ src/main/java/com/target/retail/tgtproductdetails/TgtProductDetailsApplication.java
@@ -2,9 +2,10 @@ package com.target.retail.tgtproductdetails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class TgtProductDetailsApplication {
 
 	public static void main(String[] args) {
